b00526453d73b05a4d79513a67bc95a91898cb4a
mymachine <divyanshchahar@outlook.com>
2022-04-21 18:17:31 +0530

    added header[C

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
+const today = new Date().toLocaleDateString(undefined, {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+});
+
+const logStatus = "Login";
+const userName = "Divyansh Chahar";
+const userId = "divyanchahar@outlook.com";
+
+function Header() {
+  return (
+    <div className='header'>
+      <p>{today}</p>
+
+      <p>{userName}</p>
+      <p>{userId}</p>
+      <button>{logStatus}</button>
+    </div>
+  );
+}
+
+ReactDOM.render(<Header />, document.getElementById("root"));
